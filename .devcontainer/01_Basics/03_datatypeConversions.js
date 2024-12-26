1f5e1a0dfdf4afd8c3c6c0e48be64e7bae39050d
Bhavya Jain <140505299+jbhavya876@users.noreply.github.com>
2024-12-26 11:23:10 +0000

    Dtatype Conversions

---
+++ .devcontainer/01_Basics/03_datatypeConversions.js
@@ -0,0 +1,32 @@
+let score = 33;
+// console.log(typeof score);
+
+let scoreString = String(score);
+// console.log(typeof scoreString);
+// console.log(scoreString);
+
+let id = "33abc";
+// console.log(typeof id);
+
+let idNumber = Number(id);
+// console.log(typeof idNumber);
+// console.log(idNumber);
+
+let idString = String(id);
+// console.log(typeof idString);
+// console.log(idString);
+
+
+/*
+"33" -> 33
+"33abc" -> NaN
+true -> 1
+false -> 0
+*/
+
+let isLoggedIn = 1;
+console.log(typeof isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
--- .devcontainer/01_Basics/tempCodeRunnerFile.js
+++ .devcontainer/01_Basics/tempCodeRunnerFile.js
@@ -1,1 +1,6 @@
-console.log("Bhavya");
+let id = "33abc";
+// console.log(typeof id);
+// let idNumber = Number(id);
+// console.log(typeof idNumber);
+// let idString = String(id);
+// console.log(typeof idString);
