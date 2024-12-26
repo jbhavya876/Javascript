8b603c14df2a07afd5735219c0b065b47cad03fb
Bhavya Jain <140505299+jbhavya876@users.noreply.github.com>
2024-12-26 05:13:52 +0000

    var and datatypes

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 45754;
+let accountMail = "bhavya@google.com";
+let accountPassword = "2000555";
+accountCity = "Jaipur";
+
+console.log(accountId);
+console.table([accountId, accountMail, accountPassword, accountCity]);
---
+++ .devcontainer/01_Basics/02_datatype.js
@@ -0,0 +1,18 @@
+let name = "Bhavya"
+let age = 19;
+let isLogged = true;
+let state;
+
+// console.log("Bhavya");
+
+/*
+number => 2to pwr53
+bigint
+string = ""
+boolean = true/false
+undefined
+null => standaslone value
+symbol => unique value
+object
+*/
+console.log(typeof undefined);
