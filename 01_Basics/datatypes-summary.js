9517b5a89664ff410b9fc8fa801e7765a6422ee1
Bhavya Jain <140505299+jbhavya876@users.noreply.github.com>
2025-05-17 14:22:14 +0000

    memory management

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -10,7 +10,7 @@ let userEmail;
 
 const id = Symbol("123");
 const anotherId = Symbol("123");
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 const BigInteger = 212346287363n;
 
@@ -27,3 +27,25 @@ let myObj = {
 const myFunc = function() {
     console.log("Hello World");
 }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack -> (Primitive), Heap -> (Non-Primitve)
+
+let MyChannel = "BhavyaJaindotcom";
+
+let anotherChannel = MyChannel;
+anotherChannel = "Lightningdotcom";
+
+console.log(MyChannel);
+console.log(anotherChannel);
+
+let user1 = {
+  email: "user@google.com",
+  upiId : "user@ybl"
+}
+
+let user2 = user1;
+user2.email = "bhavya@google.com";
+console.log(user1.email);
+console.log(user2.email);
