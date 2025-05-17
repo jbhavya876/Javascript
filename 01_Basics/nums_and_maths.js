1aad8e7cc6cab155c047bf7aa532e7793be1efb9
Bhavya Jain <140505299+jbhavya876@users.noreply.github.com>
2025-05-17 20:39:37 +0000

    numbers and math

---
+++ 01_Basics/nums_and_maths.js
@@ -0,0 +1,30 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(1000);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = new Number(230.896);
+// console.log(otherNumber.toPrecision(5));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+
+// +++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(2.47));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10 + 1));
+let min = 10;
+let max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
