8424e3bd8510abc3edbfe25094185392200830fd
Bhavya Jain <140505299+jbhavya876@users.noreply.github.com>
2025-05-18 15:28:50 +0000

    arrays

---
+++ 02_basics/arrays.js
@@ -0,0 +1,21 @@
+const Arr = [0, 1, 2, 3, 4, 5];
+const heros = ["Robert Downey", "Loki", "Miles Morales"];
+
+const Arr2 = new Array(0, 1, 2, 3, 4);
+console.log(Arr[1]);
+
+// Array Methods
+Arr.push(6);
+Arr.push(9);
+Arr.pop();
+console.log(Arr);
+
+Arr.unshift(9);
+console.log(Arr);
+
+Arr.shift();
+console.log(Arr);
+
+console.log(Arr.includes(9));
+
+console.log()
