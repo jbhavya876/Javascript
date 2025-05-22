f2f1c543f53b3c5285fb63cb8744176dd3ab3827
Bhavya Jain <140505299+jbhavya876@users.noreply.github.com>
2025-05-22 12:30:59 +0000

    arrays1

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -2,20 +2,31 @@ const Arr = [0, 1, 2, 3, 4, 5];
 const heros = ["Robert Downey", "Loki", "Miles Morales"];
 
 const Arr2 = new Array(0, 1, 2, 3, 4);
-console.log(Arr[1]);
+// console.log(Arr[1]);
 
 // Array Methods
 Arr.push(6);
 Arr.push(9);
 Arr.pop();
-console.log(Arr);
+// console.log(Arr);
 
 Arr.unshift(9);
-console.log(Arr);
+// console.log(Arr);
 
 Arr.shift();
-console.log(Arr);
+// console.log(Arr);
 
-console.log(Arr.includes(9));
+// console.log(Arr.includes(9));
+// console.log(Arr.indexOf(3));
 
-console.log()
+const newArr = Arr.join();
+// console.log(typeof newArr);
+// console.log(typeof Arr);
+
+const myn1 = Arr2.slice(1, 3);
+console.log("A ", Arr2);
+console.log(myn1);
+
+const myn2 = Arr2.splice(1, 3);
+console.log("B ", Arr2);
+console.log(myn2);
